5663d808a5906a42221aac119f4c94c7cc6c6ccd
Amin <amin7x6@gmail.com>
2018-09-24 20:42:22 -0700

    add controller test coverage

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -57,6 +57,6 @@ class ProductTest < ActiveSupport::TestCase
                             price: 1,
                             image_url: "fred.gif")
       assert product.invalid?
-      assert_equal ["has already taken"], product.errors[:title]
+      assert_equal ["has already been taken"], product.errors[:title]
     end
 end

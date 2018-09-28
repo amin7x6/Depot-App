0770adb040e704cef300825a94964a8471743257
Amin <amin7x6@gmail.com>
2018-09-28 14:56:57 -0700

    sum up item quantity in cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -6,6 +6,10 @@ class StoreController < ApplicationController
     session[:counter] += 1
   end
 
+  def set_counter_to_zero
+    session[:counter] = 0
+  end
+
   def index
     @count = increment_counter
     @products = Product.order(:title)

5085ee19d12e038d54eb4149ea129951157bf857
Amin <amin7x6@gmail.com>
2018-09-28 16:03:53 -0700

    finishing the cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -17,7 +17,7 @@ class Product < ApplicationRecord
 
     # ensure that there are no line items referencing this product
     def ensure_not_referenced_by_any_line_item
-      unless line_item.empty?
+      unless line_items.empty?
         errors.add(:base, 'Line Items present')
       end
     end

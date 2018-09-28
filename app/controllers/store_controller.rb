cd0a3ed7dd8785729f0129a7bb706bfbf61d1671
Amin <amin7x6@gmail.com>
2018-09-28 08:02:24 -0700

    add session acount to store page

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,13 @@
 class StoreController < ApplicationController
+  def increment_counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] += 1
+  end
+
   def index
+    @count = increment_counter
     @products = Product.order(:title)
   end
 end

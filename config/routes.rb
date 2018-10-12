7262faedad71782e909f15ec3c5e0da96c1e6841
Amin <amin7x6@gmail.com>
2018-10-11 21:42:13 -0700

    authenticate users and add tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  get 'admin' => 'admin#index'
+
+  controller :sessions do
+    get 'login' => 'sessions/new'
+    post 'login' => 'sessions/create'
+    get 'delete' => 'sessions/destroy'
+  end
   resources :users
   resources :orders
   resources :line_items
   resources :carts
   root 'store#index', as: 'store_index'
-  
+
   resources :products do
     get :who_bought, on: :member
   end

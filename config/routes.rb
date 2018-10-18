48a01acdcc109ad4cb628bb2beac55a82e8ac734
Amin <amin7x6@gmail.com>
2018-10-17 21:27:24 -0700

    selecting the locale

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,15 @@ Rails.application.routes.draw do
     get 'delete' => :destroy
   end
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
-
   resources :products do
     get :who_bought, on: :member
+  end
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store_index', via: :all
   end
 
 end
